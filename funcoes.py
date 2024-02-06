998dbf1b1712a640d372509ded99c21e50e473c9
MarioVM <mario.medeiros@sou.inteli.edu.br>
2024-02-06 15:27:12 -0300

    adicionar funcao da resposta do universo V2

--- funcoes.py
+++ funcoes.py
@@ -1,2 +1,7 @@
 def soma_dois_numeros(x, y):
     return x + y
+
+
+def subtracao(x, y):
+    string = x - y
+    return f"achou que era {string}, mas a resposta é sempre 42"
--- ola.py
+++ ola.py
@@ -1,6 +1,11 @@
 from funcoes import soma_dois_numeros
+from funcoes import subtracao
 print("Hello worlds")
 
 resultado = soma_dois_numeros(5, 9)
 
 print(f"A soma de 5 e 9 é {resultado}")
+
+resultado2 = subtracao(9, 5)
+
+print(resultado2)
